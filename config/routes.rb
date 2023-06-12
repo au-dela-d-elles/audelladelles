dfcf3918d852065b9b8d3bd366735ecd204f6209
jean-baptiste faria <fariajeanbap@gmail.com>
2023-06-12 23:38:54 +0200

    modification des chemin et du controller static pages plus ajout des pages static notre histoire, nos partenaires

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'static_pages#Home'
   get 'static_pages/Contact'
   get 'static_pages/About'
+  get 'static_pages/Notre_histoire'
+  get 'static_pages/Nos_partenaires'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
