307592284f83c35c6fbf859fa5282d46e9ba4a09
williamdore <william.dore.62@gmail.com>
2023-06-13 10:55:19 +0200

    Add Mailer for dev, waiting for prod

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
+    after_create :welcome_send 
+
+    def welcome_send
+      UserMailer.welcome_email(self).deliver_now
+    end
 end
