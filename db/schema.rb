839d5b235b6d66d30b144c2b71b9884e7e6d9a87
mathieustrosberg <mathieustrosberg@gmail.com>
2023-06-14 13:02:59 +0200

    Change link Panier

--- db/schema.rb
+++ db/schema.rb
@@ -42,19 +42,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_192031) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-
   create_table "items", force: :cascade do |t|
     t.string "image"
     t.string "name"
     t.text "description"
     t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.decimal "price"
     t.string "image"
-
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
