fe728fea7e2299c2e821610e00a0e608e43d1744
williamdore <william.dore.62@gmail.com>
2023-06-15 09:47:31 +0200

    modifs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   require "google/cloud/storage"
-  config.active_storage.service = :google
+  config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
