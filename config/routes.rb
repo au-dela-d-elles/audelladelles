d727382cdf1e73e2a76af8f56e4155a2bd501d32
jean-baptiste faria <fariajeanbap@gmail.com>
2023-06-17 23:52:24 +0200

    ajout du dashbord admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :users
+      resources :carts
+      resources :items
+      resources :orders
+
+      root to: "users#index"
+    end
+    
   resources :orders, only: [:create, :update, :destroy]
   resources :carts, except: [:index, :new, :edit]
   resources :items
