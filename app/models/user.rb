8496822d9bb3b12a97140220533982b34563ba94
jean-baptiste faria <fariajeanbap@gmail.com>
2023-06-19 13:22:41 +0200

    fix  bug boutton new artocme vue que par admin , modif form registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :zip_code, format: { with: /\A\d+\z/, message: "should only contain numbers" }, on: [:create, :update]
-
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
+  end
+
+  def admin?
+    is_admin
   end
 
 end
