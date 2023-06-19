a41981b3e15fdbc83810b6f464555d387f9b499c
williamdore <william.dore.62@gmail.com>
2023-06-19 20:03:01 +0200

    add mailer to account creation

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,13 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
+  after_action :send_account_created_email, on: :create
+
+  private
+
+  def send_account_created_email
+    UserMailer.welcome_email(resource).deliver_now
+  end
 
   # GET /resource/sign_up
   # def new
