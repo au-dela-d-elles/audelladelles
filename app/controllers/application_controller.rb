b89ef41ccbdea2ed89a94f2f55e6f01416ace1cb
jean-baptiste faria <fariajeanbap@gmail.com>
2023-06-21 13:38:21 +0200

    essaie fix bug prod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_devise_parameters, if: :devise_controller?
   
   def configure_devise_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email,:password, :password_confirmation])
-    devise_parameter_sanitizer.permit(:account_update, keys:  [:email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :fisrt_name, :last_name, :street, :city, :zip_code, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys:  [:email,:fisrt_name, :last_name, :street, :city, :zip_code, :password, :password_confirmation])
   end
   
   after_action :create_cart_and_assign_to_current_user, only: [:create]
